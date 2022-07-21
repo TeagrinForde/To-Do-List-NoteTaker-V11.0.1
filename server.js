bf7f2bd12b67a7ec876845747b22c9833279d412
Teagrin Forde <teagrinb@gmail.com>
2022-07-21 10:00:45 -0400

    fixed server.js post function

--- server.js
+++ server.js
@@ -12,60 +12,62 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));//connecting to static folder
 
 app.get('/', (req, res) => {
-    console.info(`${req.method} request received to get HTML.`);
-    res.sendFile(path.join(__dirname, '/public/index.html'))//connect to HTML
+  console.info(`${req.method} request received to get HTML.`);
+  res.sendFile(path.join(__dirname, '/public/index.html'))//connect to HTML
 });
 
 app.get('/notes', (req, res) => {
-    console.info(`${req.method} request received to get notes`);
-    res.sendFile(path.join(__dirname, '/public/notes.html')); //connect to notes
+  console.info(`${req.method} request received to get notes`);
+  res.sendFile(path.join(__dirname, '/public/notes.html')); //connect to notes
 });
 
 
 app.get('/api/notes', (req, res) => {
-    let response = fs.readFileSync('./db/db.json');
-    response = JSON.parse(response);
-    res.json(response);
-    console.log(response);
+  let response = fs.readFileSync('./db/db.json');
+  response = JSON.parse(response);
+  res.json(response);
+  console.log(response);
 });
 
 app.post('/api/notes', (req, res) => {
-    res.json(`${req.method} request received`);
-    const { title, text } = req.body;
-    
-    if (title && text) {        
-        const newNote = {
-            title,
-            text,
-        };
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-              console.error(err);
-            } else {
-              const parsedNotes = JSON.parse(data); // Convert string to JSON  
-              parsedNotes.push(newNote);// Add a new review
-      
-              fs.writeFile( // Write updated reviews back to the file
-                './db/db.json',
-                JSON.stringify(parsedNotes, null, 2),
-                console.info('Posted Note!'));
-            }
-          });
+  console.info(`${req.method} request received`);
+  const { title, text } = req.body;
 
-          const response = {
-            status: 'success',
-            body: newNote,
-          };
-    } else {
-        res.json('Notes must include content');
-    };    
-    console.log(req.body);
+  if (title && text) {
+    const newNote = {
+      title,
+      text,
+    };
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        const parsedNotes = JSON.parse(data); // Convert string to JSON  
+        parsedNotes.push(newNote);// Add a new review
+
+        fs.writeFile( // Write updated reviews back to the file
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 2),
+          (throwError) =>
+            throwError ? console.error(throwError) : console.info('Posted Note!'));
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+    console.log(response);
+  res.status(201).json(response);
+  } else {
+    res.status(500).json('Notes must include content');
+  };  
 });
 
-// app.get('*', (req, res) => {
-//     console.info(`${req.method} request redirected...`)  
-//     res.sendFile(path.join(__dirname, '/public/index.html')) //redirect to correct port
-// });
+app.get('*', (req, res) => {
+    console.info(`${req.method} request redirected...`)  
+    res.sendFile(path.join(__dirname, '/public/index.html')) //redirect to correct port
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
